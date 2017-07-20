68e4ad99b0973aaac45ca83e7a5514eba8e7c56c
wajdibenabdallah <wajdi.benabdallah@smarttouchtunisie.com>
2017-07-20 13:09:10 +0100

    FrontEnd - setMode

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,30 +8,36 @@
  * Controller of the moviesshowApp
  */
 
-myApp.config(function ($stateProvider) {
+const VERSION = "1.0.0";
 
+const urlStaticData = 'scripts/controllers/data/films.json';
+const urlDynamicData = "http://localhost:3000/api/movies";
+
+var issetMode = false;
+var testURL = null;
+
+myApp.config(function ($stateProvider) {
     $stateProvider
+        .state('home', {
+            url: "",
+            templateUrl: '/index.html'
+        })
         .state('film', {
             url: "/film/:filmId",
-            views:{
+            views: {
                 'main': {
                     templateUrl: '/views/film_details.html',
-                    controller: function ($stateParams) {
-                        // If we got here from a url of /contacts/42
-                        console.dir($stateParams);
-                    }
+                    controller: 'filmController'
                 }
             }
-
         })
 });
 
 myApp.controller('MainCtrl', function ($scope, $http, $translate) {
+    $scope.version = VERSION;
+
     $scope.data = {};
     $scope.data.films = [];
-
-    const urlStaticData = 'scripts/controllers/data/films.json';
-    const urlDynamicData = "http://localhost:3000/api/movies";
 
     $http({
         method: 'GET',
@@ -48,11 +54,48 @@ myApp.controller('MainCtrl', function ($scope, $http, $translate) {
         console.log(lang);
         $translate.use(lang);
     }
+
+    $scope.setMode = function (M) {
+        switch (M) {
+            case 'D': {
+                testURL = urlDynamicData;
+                break;
+            }
+            case 'S': {
+                testURL = urlStaticData;
+                break;
+            }
+            default: {
+                console.log('Mode Error');
+                return;
+            }
+        }
+        //show page
+        angular.element("#navBar").removeAttr("hidden");
+        angular.element("#mainPage").removeAttr("hidden");
+
+        //call service ...
+    }
 });
 
-myApp.controller('filmController', function ($scope, $stateParams) {
-    console.dir($stateParams);
-    console.log('hello world');
+myApp.controller('filmController', function ($scope, $http, $stateParams) {
+    var id = $stateParams.filmId;
+    var getFilmUrl = urlDynamicData + '/' + id;
+    //get Params
+    $http({
+        method: 'GET',
+        url: getFilmUrl
+    }).then(function (success) {
+        success.data.note = filtreNote(success.data.note);
+        $scope.selectedfilm = success.data;
+    }, function (error) {
+        console.dir(error);
+    });
+
+    function filtreNote(note) {
+        //Note sur 10
+        return Math.round(note / 2);
+    }
 });
 
 myApp.directive('ngFilm', function () {
